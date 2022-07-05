a1df6f619e7c265d8e8c7b444882d5eb4db8477f
vaishali <51531318+rana-vaishali@users.noreply.github.com>
2022-07-05 13:13:31 +0530

    Checkpoint 3

---
+++ moviebooking/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get("/movies",(req, res, next)=>{
+    res.send("All Movies Data in JSON format from Mongo DB")
+})
+app.get("/genres",(req, res, next)=>{
+    res.send("All genres Data in JSON format from Mongo DB")
+})
+app.get("/artists",(req, res, next)=>{
+    console.log("All artists Data in JSON format from Mongo DB")
+})
+
+app.listen(9000);
